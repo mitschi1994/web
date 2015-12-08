99c2ee1425d55e513968bdf6ba60be6f518b79fa
mitschi1994 <mitschi1994@users.noreply.github.com>
2015-12-07 22:51:20 -0600

    Creacion de funciones mas del modelo correo
    creacion de la funcion get_allenv encargada de mostrar los correos envi,ados segun el usuario logueado,funcion insertar encargada de insertar en la base de datos correos , funcion seleccionardestinarios que esta selecciona destinatario segun id, funcion selecconar usuario encargado de seleccionar un usuario segun su id, funcion cargar correos segun el id de ese correo, funcion elimiarcorreo encargada de la eliminacion de un correo.

--- CodeIgniter3/application/models/correos_model.php
+++ CodeIgniter3/application/models/correos_model.php
@@ -23,6 +23,75 @@ class Correos_model extends CI_Model {
    $q = $this ->db-> get('correo');
     	return $q->result();
     }
+    
+    public function get_allenv() 
+    {
+        $id = $this->user->buscarID();
+        $this ->db-> select('*');
+        $this ->db-> where('estado', 1);
+        $this ->db-> where('usuarioid', $id);
+        $q = $this ->db-> get('correo');
+        return $q->result();
+    }
+
+    function insert($correos,$destino,$fecha){
+
+        if ( $this->db->insert('correo',$correos) )
+        {
+
+            $this ->db-> select('*');
+            $this ->db-> where('fechacreacion',$fecha);
+
+            $idcorreo = $this ->db-> get('correo');
+            $res=$idcorreo->result();
+            foreach ($res as $value) {
+                $idcorreo = $value->id;
+            }
+            $data = array('destino' => $destino, 'idcorreo' => $idcorreo);
+            $this->db->insert('destinatarios',$data);
+            return True;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    function seleccionardestinatario($id){
+        $this ->db-> select('*');
+        $this ->db-> where('idcorreo', $id);
+
+        $destino = $this ->db-> get('destinatarios');
+
+        return $destino->result();   
+    }
+
+    function seleccionarusuario($idusuario){
+        $this ->db-> select('*');
+        $this ->db-> where('id', $idusuario);
+
+        $usuario = $this ->db-> get('usuario');
+
+        return $usuario->result();      
+    }
+    public function cargarcorreo($idcorreo)
+    {
+        $this ->db-> select('*');
+        $this ->db-> where('id', $idcorreo);
+        $q = $this ->db-> get('correo');
+        return $q->result();
+    }
+    public function eliminarcorreo($id)
+    {
+        $this->db->where('id', $id);
+        
+        if ($this->db->delete('correo')) {
+             return True;
+         }else{
+            return false;
+         }
+    }
 
     
 
