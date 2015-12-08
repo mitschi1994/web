4c6f764ce6ecfbcd6799540ef7388e594ef8f7e5
mitschi1994 <mitschi1994@users.noreply.github.com>
2015-12-07 22:38:24 -0600

    Mas funciones modelo Usuario
    Creacion funcion get:all para traer todo los datos de la tabla usuario, funcion autentificar revisando en la base de datos si este existe para mostrar todos sus resultados y que si el usuario ya esta activado se puede editaar, creacion funcion salir sesion,

--- CodeIgniter3/application/models/user_model.php
+++ CodeIgniter3/application/models/user_model.php
@@ -8,6 +8,51 @@ class User_model extends CI_Model {
         parent::__construct();
         $this->load->database();
     }
+    
+        function get_all() 
+    {
+      $query = $this->db->get('usuario');
+      return $query->result();
+    }
+
+    function autenticar($user, $pass) 
+    {
+
+        $this ->db-> select('*');
+        $this ->db-> where('usuario', $user);
+        $this ->db-> where('password', $pass);
+        $this ->db-> where('estado', 1);
+ 
+   $query = $this ->db-> get('usuario');
+   $result = $query->result();
+   if($result)
+   {
+    $data = array('sesion'=>1);
+    $query= $this ->db-> update('usuario', $data, "usuario='$user'");
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+        
+   }
+
+   function salir(){
+    $data = array('sesion'=>0);
+    $query= $this ->db-> update('usuario', $data, "sesion='1'");
+    
+   if($query)
+   {
+    return true;
+   }
+   else
+   {
+    return false;
+   }
+   }
+    
+    
 
 
 }
