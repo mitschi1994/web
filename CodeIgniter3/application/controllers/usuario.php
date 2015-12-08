3e4f84090bd3d7a8136c9e01384dc8bc61d45b53
mitschi1994 <mitschi1994@users.noreply.github.com>
2015-12-07 22:24:33 -0600

    Creacion de funciones
    Creacion de funcion auntentificar usuario, funcion llamando vista registro de usuario, si el registro falla, creacion de la funcion registrar un usuario, creacion funcion confirmacion , funcion  correo encargada de filtrar los correos enviados o de salida segun el usuario logueado, funcion crear correo encargada de crear un nuevo correo

--- CodeIgniter3/application/controllers/usuario.php
+++ CodeIgniter3/application/controllers/usuario.php
@@ -27,6 +27,103 @@ class Usuario extends CI_Controller {
    $this->load->view('utilizable/headers');
    $this->load->view('usuario/login');
  }
+ 
+  public function loginfail()
+ {
+ 	$this->load->view('utilizable/headers');
+ 	$this->load->view('usuario/loginfail');
+ }
+
+ public function autenticar()
+	{
+		$pass= md5($this->input->post('contrasena'));
+		$result = $this->user->autenticar($this->input->post('usuario'), $pass);
+
+		if ($result) {
+			redirect('correo', 'refresh');
+		}else{
+			redirect('fail', 'refresh');
+			
+		}
+	}
+
+ public function registro()
+ {
+ 	$this->load->view('utilizable/headers');
+ 	$this->load->view('usuario/registro');
+ }
+
+ public function registrofail()
+ {
+ 	$this->load->view('utilizable/headers');
+ 	$this->load->view('usuario/registrofail');
+ }
+
+ public function registrar()
+	{
+		$data = array(
+            'usuario' => $this->input->post('usuario'),
+            'password' => md5($this->input->post('password')),
+            'nombre' => $this->input->post('nombre'),
+			'email' => $this->input->post('email'),
+            'estado' => 0
+        );
+        if($this->user->registrar($data))
+        {
+
+        $user = $this->input->post('usuario');
+        	//$this->email->initialize($configGmail);
+ 		$this->email->set_newline("\r\n");
+		$this->email->from('michelleramrezflores@gmail.com', 'DoNotReply@michelleramrezflores@gmail.com');
+		$this->email->to($this->input->post('email'));
+		$this->email->subject('Confirmacion');
+		$this->email->message("Click en este link para verificar su correo
+								/http://localhost/CodeIgniter/usuario/confirmacion?username=$user");
+		if ($this->email->send()) {
+			echo "Favor confirmar su correo";
+		}
+		//con esto podemos ver el resultado
+		else show_error($this->email->print_debugger());
+
+        redirect('login', 'refresh');
+        }else{
+			redirect('failreg', 'refresh');
+		}
+	}
+
+	public function confirmacion(){
+		$user = $_GET['username'];
+
+		$confirm = $this->user->confirmar($user);
+
+		if ($confirm) {
+			redirect('login', 'refresh');
+		}
+	}
+
+ public function correo()
+ {
+ 	$correos = $this->Correos_model->get_all();	
+ 	$correoenv = $this->Correos_model->get_allenv();	
+ 	$data['correosa'] = $correos;
+ 	$data['correoenv'] = $correoenv;
+ 	$this->load->view('utilizable/headers');
+ 	$this->load->view('usuario/correo',$data);
+ }
+
+ public function salir()
+ {
+ 	if ($salir = $this->user->salir()) {
+ 		$this->load->view('utilizable/headers');
+ 		$this->load->view('usuario/login');
+ 	}
+ }
+
+ public function crearcorreo()
+ {
+ 	$this->load->view('utilizable/headers');
+ 	$this->load->view('usuario/crear');
+ }
 
 
 ?>
